4c147913025f38a07d848f8780b8cb412a0481db
linneaholm <a.linnea.holm@gmail.com>
2017-04-02 13:10:00 +0200

    user-div:ar

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="user">
     <h1>{{ msg }} <span>{{ id }}</span></h1>
+    <hr>
+    <div class="smallBox"></div>
+    <div class="smallBox"></div>
+    <div class="smallBox"></div>
+    <div class="bigBox"></div>
+    <hr>
     <ul>
       <li><a href="/#/">Back Home</a></li>
     </ul>
@@ -21,8 +27,36 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
+.user {
+  max-width: 800px;
+  margin: 0 auto;
+  }
+.bigBox {
+  width: 96%;
+  height: 300px;
+  background-color: #f0f0f0;
+  margin: 6px auto;
+}
+.smallBox {
+  width: 30%;
+  height: 180px;
+  background-color: #f0f0f0;
+  display: inline-block;
+  margin: 10px;
+  }
+hr {
+  border-top: #f0f0f0;
+  border-left: none;
+  border-right: none;
+  padding: 10px;
+  width: 100%;
+  }
+h1, h2 {
+  font-weight: normal;
+  }
+h4 {
+    font-weight: lighter;
+    font-style: italic;
   }
 
   ul {
