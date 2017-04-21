ee237ca6e92c712ba15fc31c7015771ee55f365d
Viktor Ceder <vceder@kth.se>
2017-04-21 15:27:15 +0200

    Poop

--- src/store.js
+++ src/store.js
@@ -8,12 +8,14 @@ export default new Vuex.Store({
   state: {
     user: false,
     logged_in: false,
-    userDb: false
+    userDb: false,
+    lastWeekTWeets: [0,0,1,0,1,0,0]
   },
   getters: {
     user: state => state.user,
     logged_in: state => state.logged_in,
-    userDb: state => state.userDb
+    userDb: state => state.userDb,
+    lastWeekTWeets: state => state.lastWeekTWeets
   },
   mutations: {
     setUser(state, user) {
@@ -22,6 +24,14 @@ export default new Vuex.Store({
     },
     setLoggedIn(state, value) {
       state.logged_in = value
+    },
+    uppdateLastWeekTweets(state, tweets) {
+      console.log("HEJ");
+      state.lastWeekTWeets = tweets
+      console.log('LastWeek');
+    },
+    getLastWeekTweets(state){
+      return state.lastWeekTWeets
     }
   }
 })
